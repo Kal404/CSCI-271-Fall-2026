8ee0c365f350f89cb33d5bb51f8f8b3b5d9096c2
kal404 <khaled.ahmed933@login.cuny.edu>
2026-09-08 21:15:26 -0400

    Adding hello.cpp for Assignment1

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string name;
+	cout << "Hello, world!" << endl;
+	cout << "What is your name? ";
+	cin >> name;
+	cout << "\nNice to meet you, " << name;
+	
+	return 0;
+}
